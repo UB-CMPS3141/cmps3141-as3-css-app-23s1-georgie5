9b0ab2a2318d98bf97822d92f6266237b7265911
georgie5 <2021154499@ub.edu.bz>
2023-09-22 23:53:26 -0600

    final commit

--- expenses/index.js
+++ expenses/index.js
@@ -16,6 +16,8 @@ globalThis.app = createApp({
 		totalPersonBOwes: 0,
 		nowDate: new Date().toISOString().slice(0,10),
 		errors: "",
+		showConfirmationDialog: false,
+		expenseToAdd: null,
 		
 		form: {
 			payer: "",
@@ -48,94 +50,124 @@ globalThis.app = createApp({
 			return amount * rates[to] / rates[from];
 		},
 
-		addPayment() {
-			const payer = this.form.payer;
-			const payingTo = this.form.payingTo;
-			const description = this.form.description;
-			const pDate = this.form.pDate;
-			const currency = this.form.currency;
-			const formAmount = this.form.amount.toFixed(2);
-			let amount = this.form.amount.toFixed(2);
-		  
-			// Check if any of the required fields are empty or the amount is zero
-			if (payer === "" || payingTo === "" || formAmount === "0.00" || description === "" || pDate === "") {
-			  this.errors = "* Please fill out all the fields!";
-			  return false;
+		addExpense() {
+			// Check if any required fields are missing
+			if (!this.form.payer || !this.form.payingTo || !this.form.amount || !this.form.pDate) {
+			  this.errors = "*Please fill out missing fields";
+			  return; // Exit the method if any required fields are missing
 			}
 		  
-			// Currency conversion (you can customize this part)
-			if (currency === "MXN") {
-			  amount = (this.currencyConvert("MXN", "BZD", amount)).toFixed(2);
-			} else if (currency === "GTQ") {
-			  amount = (this.currencyConvert("GTQ", "BZD", amount)).toFixed(2);
-			}
+			// Create an expense object from the form data
+			const expense = {
+			  pDate: this.formatDate(this.form.pDate),
+			  payer: this.form.payer,
+			  payingTo: this.form.payingTo,
+			  currency: this.form.currency,
+			  formAmount: this.form.amount,
+			  description: this.form.description,
+			  personAOwes: 0, // Initialize to 0
+			  personBOwes: 0, // Initialize to 0
+			};
 		  
-			// Confirm before adding the payment
-			if (confirm("Are you sure you want to add this payment? If so, click 'OK'")) {
-			  // Calculate personAOwes and personBOwes based on payer and payingTo
-			  let personAOwes = 0;
-			  let personBOwes = 0;
+			// Add the expense to the expenses array
+			this.expenses.push(expense);
 		  
-			  if (payer === this.personB && (payingTo === this.personA || payingTo === this.joint)) {
-				personAOwes = (amount / 2).toFixed(2);
-				personBOwes = (amount / 2).toFixed(2);
-			  } else if (payer === this.personA && (payingTo === this.personB || payingTo === this.joint)) {
-				personAOwes = (amount / 2).toFixed(2);
-				personBOwes = (amount / 2).toFixed(2);
-			  } else if (payer === this.joint && (payingTo === this.personA || payingTo === this.personB)) {
-				personAOwes = 0;
-				personBOwes = 0;
+			// Update the total amounts owed for each person
+			if (expense.payer === this.personA) {
+			  // Neo paid, update Trinity's owes
+			  if (expense.payingTo === this.personB) {
+				expense.personBOwes = this.currencyConvert(
+				  expense.currency,
+				  "BZD",
+				  expense.formAmount
+				);
+				this.totalPersonAOwes += expense.personBOwes;
 			  }
-		  
-			  // Update total amounts
-			  this.totalPersonAOwes += Number(personAOwes);
-			  this.totalPersonBOwes += Number(personBOwes);
-		  
-			  // Add the payment to expenses array
-			  this.expenses.push({
-				payer,
-				payingTo,
-				amount,
-				description,
-				pDate,
-				personAOwes,
-				personBOwes,
-				currency,
-				formAmount
-			  });
-		  
-			  // Reset the form and clear errors
-			  this.form = {
-				payer: "",
-				payingTo: "",
-				amount: 0,
-				description: "",
-				pDate: "",
-				currency: "BZD"
-			  };
-			  this.errors = "";
-			} else {
-			  return false;
+			} else if (expense.payer === this.personB) {
+			  // Trinity paid, update Neo's owes
+			  if (expense.payingTo === this.personA) {
+				expense.personAOwes = this.currencyConvert(
+				  expense.currency,
+				  "BZD",
+				  expense.formAmount
+				);
+				this.totalPersonBOwes += expense.personAOwes;
+			  }
+			} else if (expense.payer === this.joint) {
+			  // Joint expense, update both persons' owes
+			  if (expense.payingTo === this.personA) {
+				const amount = this.currencyConvert(
+				  expense.currency,
+				  "BZD",
+				  expense.formAmount / 2
+				);
+				expense.personAOwes = amount;
+				expense.personBOwes = amount;
+				this.totalPersonAOwes += amount;
+				this.totalPersonBOwes += amount;
+			  } else if (expense.payingTo === this.personB) {
+				const amount = this.currencyConvert(
+				  expense.currency,
+				  "BZD",
+				  expense.formAmount / 2
+				);
+				expense.personAOwes = amount;
+				expense.personBOwes = amount;
+				this.totalPersonAOwes += amount;
+				this.totalPersonBOwes += amount;
+			  }
 			}
-		  }
 		  
+			// Clear the form fields
+			this.form.payer = "";
+			this.form.payingTo = "";
+			this.form.amount = 0;
+			this.form.description = "";
+			this.form.pDate = "";
+		  
+			// Reset errors
+			this.errors = "";
+		  },
+		  
+		  formatDate(dateString) {
+			// Check if the input date string is in "yyyy-mm-dd" format
+			const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+			if (!dateRegex.test(dateString)) {
+			  // Invalid date format, return an empty string or handle it as needed
+			  return "";
+			}
+		  
+			// Create a Date object in the local time zone
+			const date = new Date(dateString);
+		  
+			// Add one day to the date to account for the offset
+			date.setDate(date.getDate() + 1);
+		  
+			const day = date.getDate().toString().padStart(2, '0');
+			const month = (date.getMonth() + 1).toString().padStart(2, '0');
+			const year = date.getFullYear();
+			return `${day}/${month}/${year}`;
+		  },
+
 
 	},
 
+
 	computed: {
-		total_balance () {
-			let total = 0;
-
-			for (let expense of this.expenses) {
-				let trinity_paid = expense.trinity_paid ?? 0;
-				let neo_paid = expense.neo_paid ?? 0;
-				let trinity_paid_for_neo = expense.trinity_paid_for_neo ?? 0;
-				let neo_paid_for_trinity = expense.neo_paid_for_trinity ?? 0;
-
-				total += (trinity_paid - neo_paid)/2 + trinity_paid_for_neo - neo_paid_for_trinity;
-			}
-
-			return total;
+		total_balance() {
+		  let totalPersonAOwes = 0;
+		  let totalPersonBOwes = 0;
+	  
+		  for (let expense of this.expenses) {
+			totalPersonAOwes += expense.personAOwes;
+			totalPersonBOwes += expense.personBOwes;
+		  }
+	  
+		  return {
+			totalPersonAOwes,
+			totalPersonBOwes
+		  };
 		}
-	}
+	  }
+	  
 }, "#app");
